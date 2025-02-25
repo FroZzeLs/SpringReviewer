f9e8a2d994405daf44eb74b3e3e718cc2c536c49
Sergey <egorenkovs12@gmail.com>
2025-02-25 11:53:29 +0300

    fix sonar bugs

--- src/test/java/by/frozzel/springreviewer/SpringReviewerApplicationTests.java
+++ src/test/java/by/frozzel/springreviewer/SpringReviewerApplicationTests.java
@@ -2,6 +2,19 @@ package by.frozzel.springreviewer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class SpringReviewerApplicationTests { }
+class SpringReviewerApplicationTests {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
+	}
+
+}
