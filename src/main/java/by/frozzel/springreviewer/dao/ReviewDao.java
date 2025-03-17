672e65949b214ed7cadec5e48bdf27a34c999d12
FroZzeLs <egorenkovs12@gmail.com>
2025-03-17 23:49:22 +0300

    add checkStyle

--- src/main/java/by/frozzel/springreviewer/dao/ReviewDao.java
+++ src/main/java/by/frozzel/springreviewer/dao/ReviewDao.java
@@ -1,29 +1,31 @@
 package by.frozzel.springreviewer.dao;
 
 import by.frozzel.springreviewer.model.Review;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public class ReviewDAO {
+public class ReviewDao {
     private final List<Review> reviews = List.of(
             new Review(1, 1, "Лучший преподаватель java!"),
             new Review(2, 1, "Весёлый дед, норм поясняет за предмет, помогает с лабами"),
             new Review(3, 3, "Как эта бабка проходит психолога на медкомиссии...")
     );
 
-    public List<Review> findByTeacherId(int teacherId) {
-        return reviews.stream()
-                .filter(r -> r.getTeacherId() == teacherId)
-                .toList();
+    public Optional<List<Review>> findByTeacherId(int teacherId) {
+        List<Review> result = reviews.stream()
+                .filter(review -> review.getTeacherId() == teacherId)
+                .collect(Collectors.toList());
+
+        return result.isEmpty() ? Optional.empty() : Optional.of(result);
     }
 
-    public Review findById(int id) {
+    public Optional<Review> findById(int id) {
         return reviews.stream()
-                .filter(r -> r.getId() == id)
-                .findFirst()
-                .orElse(null);
+                .filter(review -> review.getId() == id)
+                .findFirst();
     }
 
     public List<Review> findAll() {
--- src/main/java/by/frozzel/springreviewer/service/ReviewService.java
+++ src/main/java/by/frozzel/springreviewer/service/ReviewService.java
@@ -1,28 +1,33 @@
 package by.frozzel.springreviewer.service;
 
-import by.frozzel.springreviewer.dao.ReviewDAO;
+import by.frozzel.springreviewer.dao.ReviewDao;
 import by.frozzel.springreviewer.model.Review;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ReviewService {
-    private final ReviewDAO reviewDAO;
+    private final ReviewDao reviewDao;
 
-    public ReviewService() {
-        this.reviewDAO = new ReviewDAO();
+    public ReviewService(ReviewDao reviewDao) {
+        this.reviewDao = reviewDao;
     }
 
     public List<Review> getReviewsByTeacher(int teacherId) {
-        return reviewDAO.findByTeacherId(teacherId);
+        return reviewDao.findByTeacherId(teacherId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Отзывы для данного преподавателя не найдены"));
     }
 
     public Review getReviewById(int id) {
-        return reviewDAO.findById(id);
+        return reviewDao.findById(id)
+                .orElseThrow(() -> new
+                        ResponseStatusException(HttpStatus.NOT_FOUND, "Отзыв не найден"));
     }
 
     public List<Review> getAllReviews() {
-        return reviewDAO.findAll();
+        return reviewDao.findAll();
     }
 }
