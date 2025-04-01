2254e03b0e4122b0a638a09d78e8af391b9f90c2
FroZzeLs <egorenkovs12@gmail.com>
2025-04-02 00:24:13 +0300

    fix sonar

--- src/main/java/by/frozzel/springreviewer/mapper/ReviewMapper.java
+++ src/main/java/by/frozzel/springreviewer/mapper/ReviewMapper.java
@@ -29,14 +29,14 @@ public class ReviewMapper {
         review.setComment(dto.getComment());
 
         User user = userRepository.findById(dto.getUserId())
-                .orElseThrow(() -> new RuntimeException("User"
-                        + " not found with ID: " + dto.getUserId()));
+                .orElseThrow(() -> new RuntimeException("User not found with ID: "
+                        + dto.getUserId()));
         Teacher teacher = teacherRepository.findById(dto.getTeacherId())
-                .orElseThrow(() -> new RuntimeException("Teacher"
-                       + " not found with ID: " + dto.getTeacherId()));
+                .orElseThrow(() -> new RuntimeException("Teacher not found with ID: "
+                        + dto.getTeacherId()));
         Subject subject = subjectRepository.findById(dto.getSubjectId())
-                .orElseThrow(() -> new RuntimeException("Subject"
-                       + " not found with ID: " + dto.getSubjectId()));
+                .orElseThrow(() -> new RuntimeException("Subject not found with ID: "
+                        + dto.getSubjectId()));
 
         review.setUser(user);
         review.setTeacher(teacher);
